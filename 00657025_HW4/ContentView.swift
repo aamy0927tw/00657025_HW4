d3e2ffae19606ffe5f5fa9863b1b33d8ff526f1f
“Your <goodjob132@yahoo.com.tw>
2020-05-14 15:02:05 +0800

    Initial Commit

---
+++ 00657025_HW4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  00657025_HW4
+//
+//  Created by User23 on 2020/5/14.
+//  Copyright © 2020 User23. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
